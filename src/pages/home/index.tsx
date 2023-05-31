e5e56e57c16f723831725efefb00078e5f6edfe9
Marcelo Martins de Oliveira <mmoliveira@gmail.com>
2023-05-30 22:43:12 -0300

    Inicio de projeto

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export function Home(){
+    return (
+        <div>
+            <h1>Bem vindo a página Home!</h1>
+            <span>Essa é a minha primeira página com navegação</span>
+            <br />
+            <Link to="/sobre">Sobre</Link>
+        </div>
+        )
+}
---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export function Sobre(){
+    return (
+        <div>
+            <h1>Página Sobre!</h1>
+            <br />
+            <Link to="/">Home</Link>
+        </div>
+        )
+}
